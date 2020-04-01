747152a94580b4dd9f31992047c00ce63d8e8f50
bruce willis <fashun_vyctum@yahoo.com>
2020-04-01 22:49:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  if array.empty?
+  if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{array[0]}."
