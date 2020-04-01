453c831e522ec97fc98cc4df231efd9317553f4b
bruce willis <fashun_vyctum@yahoo.com>
2020-04-01 20:55:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,8 @@ def line(katz_deli)
   line = "The line is currently: "
   if katz_deli.length > 0
     katz_deli.each_with_index do |name, index|
-      line += 
+      line += "#{index + 1}. #{name} "
+      
   puts "The line is currently empty."
   
 end
