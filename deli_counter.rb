7a60e55bef52688f1b4211d5d36481b372d94abf
bruce willis <fashun_vyctum@yahoo.com>
2020-04-01 21:45:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,15 +3,10 @@ def line(katz_deli)
   line = "The line is currently: "
   if katz_deli.length > 0
     katz_deli.each_with_index do |name, index|
-      line += "#{index + 1}. #{name}"
+      line += " #{index + 1}. #{name}"
     end
     puts line
   else
   puts "The line is currently empty."
 end
-end
-
-def take_a_number(katz_deli, name)
-	katz_deli.push(name)
-	puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
