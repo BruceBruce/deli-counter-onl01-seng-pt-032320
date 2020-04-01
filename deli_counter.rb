1861758fa2a5c4f611dc0448a2f7200c8661907e
bruce willis <fashun_vyctum@yahoo.com>
2020-04-01 22:49:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ def take_a_number(katz_deli, name)
 	puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving(array)
+def now_serving(katz_deli)
   if array.empty?
     puts "There is nobody waiting to be served!"
   else
