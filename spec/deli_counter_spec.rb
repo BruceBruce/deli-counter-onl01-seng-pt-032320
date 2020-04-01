6ad2c4d65143bf21f3d52806080c1c61867d6234
bruce willis <fashun_vyctum@yahoo.com>
2020-04-01 22:21:46 +0000

    Automatically backed up by Learn

--- spec/deli_counter_spec.rb
+++ spec/deli_counter_spec.rb
@@ -75,3 +75,13 @@ describe 'Deli Counter' do
 
 end
 
+take_a_number(katz_deli, "Igor")
+take_a_number(katz_deli, "Bob")
+take_a_number(katz_deli, "Melanie")
+ line(katz_deli)
+ now_serving(katz_deli)
+ line(katz_deli)
+ take_a_number(katz_deli, "Corgi")
+ line(katz_deli)
+ now_serving(katz_deli)
+ line(katz_deli)
