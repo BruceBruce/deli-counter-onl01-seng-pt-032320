3326e3415dab47d157d54397922d22045855e072
bruce willis <fashun_vyctum@yahoo.com>
2020-04-01 21:46:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
   line = "The line is currently: "
   if katz_deli.length > 0
     katz_deli.each_with_index do |name, index|
-      line += " #{index + 1}. #{name}"
+      line += "#{index + 1}. #{name}"
     end
     puts line
   else
